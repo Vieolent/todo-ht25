0398acfd0342ecab37d2e0d17615dc35124a0f79
Vieolent <miranda.l.bache@gmail.com>
2025-11-19 15:40:45 +0100

    working crud

--- app.rb
+++ app.rb
@@ -8,8 +8,46 @@ require 'sinatra/reloader'
 
 
 # Routen /
-get '/' do
+get('/') do
+    db = SQLite3::Database.new("db/todos.db")
+    db.results_as_hash = true
+    @todos = db.execute("SELECT * FROM todos")
+
     slim(:index)
 end
 
+post("/:id/delete") do
+    db = SQLite3::Database.new("db/todos.db")
+    removing = params[:id].to_i
+    db.execute("DELETE FROM todos WHERE id = ?", removing)
+    redirect('/')
+end
 
+get('/new') do 
+    slim(:new)
+end
+
+post('/new') do
+    new_todo = params[:new_todo]
+    description = params[:description]
+    db = SQLite3::Database.new("db/todos.db")  
+    db.execute("INSERT INTO todos (name, description) VALUES (?, ?) ", [new_todo, description])
+    redirect('/')
+end
+
+get("/:id/edit") do
+    id = params[:id].to_i
+    db = SQLite3::Database.new("db/todos.db")
+    db.results_as_hash = true
+    @selected_todo = db.execute("SELECT * FROM todos WHERE id=?", id).first
+    slim(:"/edit")
+end
+
+post("/:id/update") do
+    db = SQLite3::Database.new("db/todos.db")  
+    id = params[:id].to_i
+    name = params[:name]
+    description = params[:description]
+    db.execute("UPDATE todos SET name=?, description=? WHERE id=?", [name, description, id])
+    redirect('/')
+end
